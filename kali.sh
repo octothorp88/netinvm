0540dee749f9169faa87f2023368f4fa386884a0
Kali <octothorp@local>
2020-06-21 08:54:43 -0400

    added audacity

--- kali.sh
+++ kali.sh
@@ -304,6 +304,7 @@ pull_git_repo https://github.com/Paradoxis/StegCracker.git /opt/stegcracker "Ste
     install_apt_pkg code "Microsoft Visual Studio Code"
     install_apt_pkg powershell "powershell"
     install_apt_pkg seclists "seclists"
+    install_apt_pkg audacity "audacity"
 
 
     if [ $(dpkg-query -W -f='${Status}' docker-ce 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
