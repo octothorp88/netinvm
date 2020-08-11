0220dbdb656c53663728b6768f771f9249ecfe76
Kali <octothorp@local>
2020-08-11 19:49:47 -0400

    added enum.sh until there is a home elsewhere for it

---
+++ enum.sh
@@ -0,0 +1,100 @@
+# First we need to identify the hosts that are online
+# The easiest thing is to find the systems that respond
+# to ICMP
+
+if [ ! -x "$(command -v nmap)" ];then
+        echo "[+] nmap not detected...Installing"
+        sudo apt-get install nmap -y >> install.log
+fi
+
+if [ ! -x "$(command -v cutycapt)" ];then
+        echo "[+] cutycapt not detected...Installing"
+        sudo apt-get install cutycapt -y >> install.log
+fi
+
+if [ ! -x "$(command -v jq)" ];then
+        echo "[+] jq not detected...Installing"
+        sudo apt-get install jq -y >> install.log
+fi
+
+if [ "$1" != "" ]; then
+    echo "Parameter 1 is set to $1"
+else
+    echo "Usage: $0 10.10.1.1-254"
+    exit
+fi
+
+if [ ! -d "$1/enum" ]; then
+    echo "making directory $1/enum"
+    mkdir -p $1/{enum,cutycapt}
+fi
+
+echo "[+] Running simple Ping sweep against $1"
+nmap -sP $1 -oA $1/PingSweep >/dev/null
+cat $1/pingsweep.gnmap  | grep Host | cut -d" " -f 2 > $1/hosts.txt
+echo "[i] Found $(wc -l $1/hosts.txt | cut -d" " -f 1) hosts"
+
+# echo [+] looking for Simple Servics with nmap
+# ports="80,443,21,22,8080,8443"
+
+# sudo nmap -sS $1 --open -p $ports -iL $1/hosts.txt -oA $1/web >/dev/null
+# echo "    $(cat $1/web.gnmap  | grep -E '\ 80\/open' | wc -l) HTTP servers"
+# echo "    $(cat $1/web.gnmap  | grep -E '\ 443\/open' | wc -l) HTTPS servers"
+# echo "    $(cat $1/web.gnmap  | grep -E '\ 8080\/open' | wc -l) proxy servers"
+# echo "    $(cat $1/web.gnmap  | grep -E '\ 8443\/open' | wc -l) 8443 servers"
+# echo "    $(cat $1/web.gnmap  | grep -E '\ 21\/open' | wc -l) ftp servers"
+# echo "    $(cat $1/web.gnmap  | grep -E '\ 22\/open' | wc -l) ssh servers"
+
+echo "[+] NMAP scans on all hosts"
+for host in $(cat $1/hosts.txt); do
+    [[ -d  $1/$host ]] || mkdir $1/$host
+    echo "[+] running nmap top 1K on $host"
+    nmap --top-ports 1000 -sV $host -oA $1/$host/${host}-top1k &> /dev/null
+    echo "[+] nmap top 1k scan complete on host $host"
+    if grep -q "445/tcp" $1/$host/${host}-top1k.nmap; then
+        echo [+] SAMBA/SMB detected
+        echo [+] Running nmap smb-enum scripts
+        nmap $host -p 445 --script=smb-enum-shares.nse,smb-enum-users.nse -oA $1/$host/${host}-smb &>/dev/null
+        echo "    smbget -R smb://<ip>/anonymous"
+        echo [+] Running nmap smb-vuln scripts
+        nmap $host -p 445 --script=smb-vuln* -oA $1/$host/${host}-smbvulns &>/dev/null
+    fi
+
+    if grep -Eoq '^[0-9]+/.*http' $1/$host/${host}-top1k.nmap; then
+        for port in $(grep -Eo '^[0-9]+/.*http' $1/$host/${host}-top1k.nmap | cut -d"/" -f1);
+        do
+            echo -ne "\r     - http found $host $port\r";sleep 2; cutycapt --url=http://$host:$port --out=$1/cutycapt/$host-$port.png>/dev/null
+        done
+        echo "[+] Identified $(grep -Eo '^[0-9]+/.*http' $1/$host/${host}-top1k.nmap | wc -l) web servers on ${host}"
+    fi
+done
+
+if ls ${1}/cutycapt/*.png &> /dev/null;
+then
+    echo "[+] Creating Viewable HTML page"
+    cat <<- EOF > $1/cutycapt.html
+    <!DOCTYPE html>
+    <html>
+        <head>
+        </head>
+        <body>
+EOF
+        for img in $(ls -1 $1/cutycapt/*.png)
+            do 
+                echo "<h2>$(basename $img)</h2>"
+                echo "<a href=\"http://${host}:${port}\">"
+                echo "<img src=\"cutycapt/$(basename $img)\">"
+                echo "</a>"
+
+        done >> $1/cutycapt.html
+
+        cat <<- EOF >> $1/cutycapt.html
+        </body>
+        </html>
+EOF
+else
+    echo "[+] No webpages detected on standard ports"
+fi
+echo "complete"
+
+
