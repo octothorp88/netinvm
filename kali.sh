436bb726b1627043d6670aa315d102878ea15bc3
Octothorp88 <octothorp@octothorp88.com>
2020-09-14 08:24:42 -0600

    added in shellpop
    Added shellpop git repo

--- kali.sh
+++ kali.sh
@@ -273,6 +273,7 @@ create_symlink /opt/msfpc/msfpc.sh ~/bin/msfpc
     pull_git_repo https://github.com/dievus/threader3000.git /opt/threader3000 "threader3000"
     pull_git_repo https://github.com/pentestmonkey/windows-privesc-check /opt/windows-privesc-check "Windows Privesc Check"
     pull_git_repo https://github.com/pentestmonkey/unix-privesc-check /opt/unix-privesc-check "unix Privesc Check"
+    pull_git_repo https://github.com/0x00-0x00/ShellPop.git /opt/shellpop "shellpop"
     # sudo pip3 install threader3000
 
     if [ ! -L "/usr/local/bin/threader3000" ] ; then
